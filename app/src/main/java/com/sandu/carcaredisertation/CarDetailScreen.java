8874008153af206e51abe06df84492ba926163d1
sandu <sandu.andrei86@gmail.com>
2020-05-22 17:29:20 +0100

    Day 1    Design for Main screen and Cardetail screen    Color setup    Strings setup    Manifest file setup

---
+++ app/src/main/java/com/sandu/carcaredisertation/CarDetailScreen.java
@@ -0,0 +1,14 @@
+package com.sandu.carcaredisertation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarDetailScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cardetail_activity);
+    }
+}
